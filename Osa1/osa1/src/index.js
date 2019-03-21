dc5f9c1da17fafd48304b43f40544f6bda9f8a18
MikaLeppiaho <MikaLeppiaho@users.noreply.github.com>
2019-03-21 16:59:38 +0200

    1.2 Kurssitiedot, step2

--- Osa1/osa1/src/index.js
+++ Osa1/osa1/src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const part1 = 'Reactin perusteet'
+const exercises1 = 10
+const part2 = 'Tiedonvälitys propseilla'
+const exercises2 = 7
+const part3 = 'Komponenttien tila'
+const exercises3 = 14
+
 const App = () =>{
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
-    const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
-    const exercises2 = 7
-    const part3 = 'Komponenttien tila'
-    const exercises3 = 14
+   
+
     
 
     return(
         <div>
             <Header course={course} /> 
-            <Content part={part1} exc={exercises1} />
-            <Content part={part2} exc={exercises2} />
-            <Content part={part3} exc={exercises3} />
-
+            <Content/>
             <Total total={exercises1+exercises2+exercises3} tehtävää />
         </div>
     )
@@ -31,10 +31,21 @@ const Header = (props) => {
     )
 
 }
-const Content = (props)=> {
+const Content = () => {
+
     return(
         <div>
-            <p>{props.part} {props.exc}</p>
+            <Part part={part1} exec={exercises1}/>
+            <Part part={part2} exec={exercises2}/>
+            <Part part={part3} exec={exercises3}/>
+        </div>
+    )
+}
+
+const Part = (props) => {
+    return(
+        <div>
+            {props.part} {props.exec}
         </div>
     )
 }
@@ -46,5 +57,7 @@ const Total = (props) => {
         </div>
     )
 }
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
