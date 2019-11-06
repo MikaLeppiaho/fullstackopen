65fc72bc9d33125f7a0357993db82dfbe00c71af
MikaLeppiaho <MikaLeppiaho@users.noreply.github.com>
2019-11-06 17:34:05 +0200

    lisätty index.js

---
+++ Osa4/index.js
@@ -0,0 +1,50 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://fullstack:s4l4sana@cluster0-t4ghf.mongodb.net/blog-app?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl, { useNewUrlParser: true,  useUnifiedTopology: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch((error) => {
+      console.log('Error: ',error.message)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch((error) => {
+      console.log('Error: ',error.message)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
