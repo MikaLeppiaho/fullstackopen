dbf67651fb8f04fb35998174dd203a884863c08e
MikaLeppiaho <MikaLeppiaho@users.noreply.github.com>
2020-06-28 13:29:59 +0300

    Tehtavat 4.22 blogilistan laajennus, step11

--- Osa4/Blogilista/app.js
+++ Osa4/Blogilista/app.js
@@ -1,39 +1,43 @@
 const express = require("express")
+require('express-async-errors')
+const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
-const app = express()
-//Blogin skeema, jossa se tallennetaan
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+const blogsRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
-const Blog = mongoose.model('Blog', blogSchema)
-const mongoUrl = 'mongodb+srv://fullstack:s4l4sana@cluster0-t4ghf.mongodb.net/blogs?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
 
-app.get('/api/blogs', ( request, response ) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
+logger.info('Connectiong to', config.MONGODB_URI)
 
-app.post('/api/blogs', ( request, response) => {
-    const blog= new Blog(request.body)
+getConnection = async () => {
+    try{
+        await mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+        logger.info('Connected to MongoDB')
+    } catch (err) {
+        logger.error('Error connecting to MongoDB: ',error.message)
+    }
+}
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+getConnection()
 
 app.use(cors())
+
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+
+app.use('/api/users', userRouter)
+app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
+
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
 
 module.exports = app
